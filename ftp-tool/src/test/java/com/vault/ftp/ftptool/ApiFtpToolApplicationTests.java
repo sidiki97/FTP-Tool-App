24f12a9055d5e46478d24835121021c7175cdd74
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2022-08-28 16:31:51 -0400

    Add files via upload

--- ftp-tool/src/test/java/com/vault/ftp/ftptool/ApiFtpToolApplicationTests.java
+++ ftp-tool/src/test/java/com/vault/ftp/ftptool/ApiFtpToolApplicationTests.java
@@ -20,42 +20,7 @@ class ApiFtpToolApplicationTests {
 	}
 
 	public static void main(String[] args) {
-//		String osname = System.getProperty("os.name");
-//		float fileSize = 10419337;
-//		if (!((System.getProperty("os.name").substring(0,3)) == "Mac")){
-//			fileSize = (fileSize / (1024*1024)) * 1000 * 1000;
-//		}
-//
-//		int filesize = (int)fileSize;
-//		System.out.println(osname);
 
-		VaultClientId vaultClientId = new VaultClientId("Veeva", "Vault",
-				"Platform", true, "FTP App");
-
-		VaultClient vaultClient;
-
-		Authentication authentication = new Authentication();
-		authentication.setPassWord("V@ultWork23");
-		authentication.setUserName("abbas.siddiqui@supportvaults.com");
-		authentication.setVaultDNS("supportvaults-platform-lr-vv2-2087-community.veevavault.com");
-
-		vaultClient = VaultClientBuilder
-				.newClientBuilder(VaultClient.AuthenticationType.BASIC)
-				.withVaultDNS(authentication.getVaultDNS())
-				.withVaultUsername(authentication.getUserName())
-				.withVaultPassword(authentication.getPassWord())
-				.withVaultClientId(vaultClientId)
-				.build();
-
-		DeleteFTP deleteFTP = new DeleteFTP();
-
-		deleteFTP.setFtpPath("u8790933");
-		deleteFTP.setRecursive(true);
-		FileStagingJobResponse fileStagingJobResponse = vaultClient.newRequest(FileStagingRequest.class)
-				.setRecursive(deleteFTP.getRecursive())
-				.deleteFolderOrFile(deleteFTP.getFtpPath());
-
-		System.out.println(fileStagingJobResponse.getData().toString());
 
 
 	}
